711ace5124cda450f4231dfec989a60fd2676e09
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-10-11 12:30:01 +0900

    Add github automation !!

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # Problem
 I want to create and register deploy keys for doing `git pull` or `git clone`. What do I do?
 
-# Solution
+# Solution (with github automation)
 1. Clone this repository.
-1. `cd tools`
-1. if there is no *deploy-key-gen.py try* `checkout develop`
+1. Create personal access tokens on (https://github.com/settings/tokens)[https://github.com/settings/tokens]
+1. Click `Generate new token`
+1. Add some note for yourself and tick `repo` (Full control of private repositories)
+1. `Generate token`
+1. Copy the token and paste in on a new file in `secret.txt` that is at the same location with the script.
 1. add/remove target repository name in *deploy-key-config.json*.
+1. edit `owner` field to your username/organization, i.e. `zynaxsoft`.
 1. `python3 deploy-key-gen.py -N [passphrase] -n [pc_name] -t [target_folder]`
   * ex. `python3 deploy-key-gen.py -N password1234 -n my_laptop -t ssh_keys`
   * passphrase is a password that you have to enter when doing `git` remote related commands
-1. Follow the appeared readme in the terminal.
+1. Follow the instructions on appeared readme in the terminal.
 
-> * Add the printed public keys (or in public-keys.txt) to the
->   appropriate repositories (Repository/Setting/Deploy Keys)
-> * Move the generated keys to ~/.ssh (both private and public)
-> * Append ~/.ssh/config with the content in ssh-config.txt
-> * When used, Set the remote url to github-[repository_name]. e.g. github-myrepo:zynaxsoft/myrepo
+
+# Solution (no github automation)
+1. Clone this repository.
+1. add/remove target repository name in *deploy-key-config.json*.
+1. `python3 deploy-key-gen.py -N [passphrase] -n [pc_name] -t [target_folder] --no-github`
+  * ex. `python3 deploy-key-gen.py -N password1234 -n my_laptop -t ssh_keys --no-github`
+  * passphrase is a password that you have to enter when doing `git` remote related commands
+1. Follow the instructions on appeared readme in the terminal.
--- deploy-key-gen.py
+++ deploy-key-gen.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 
+import requests
 
 parser = argparse.ArgumentParser(description='ssh-key generator')
 parser.add_argument('--passphrase', '-N', required=True,
@@ -10,13 +11,48 @@ parser.add_argument('--pc-name', '-n', required=True,
                     help='current PC name (could be any, required)')
 parser.add_argument('--destination', '-t', default='.',
                     help='destination folder')
+parser.add_argument('--write-access', action='store_true',
+                    help='if specified the deploy key will have the'
+                         ' write access to the repository')
+parser.add_argument('--no-github', action='store_true',
+                    help='generate key only without adding it to github')
 args = parser.parse_args()
 
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
+if not args.no_github:
+    with open(os.path.join(script_dir, 'secret.txt'), 'r') as secret_file:
+        github_secret = secret_file.readline().strip()
 json_config_file = os.path.join(script_dir, 'deploy-key-config.json')
 with open(json_config_file, 'r') as json_file:
     config_dict = json.load(json_file)
+
+
+if args.write_access:
+    print('WARNING: every keys will be written with write-access')
+
+
+def add_deploy_key(repo_name, public_key):
+    print(f'Adding key {public_key} to {repo_name}')
+    header = {'Authorization': f'token {github_secret}',
+              'Content-Type': 'application/json'}
+    url = f'https://api.github.com/repos/' \
+          f'{config_dict["owner"]}/{repo_name}/keys'
+    request_data = json.dumps({
+        'title': args.pc_name,
+        'key': public_key,
+        'read_only': not args.write_access,
+        })
+    response = requests.post(url,
+                             data=request_data,
+                             headers=header,
+                             )
+    if int(response.status_code) != 201:
+        print(f'Error adding {repo_name}: {response.content}')
+        return False
+    print('Successfully added the public key to {repo_name}')
+    return True
+
 
 print_buffer = ''
 ssh_config = ''
@@ -28,8 +64,10 @@ for repository_name in config_dict['repository']:
     os.system(shell_cmd)
     print_buffer += f'{repository_name}: \n'
     with open(f'{path}.pub', 'r') as public_key_file:
-        for line in public_key_file.readline():
-            print_buffer += line
+        public_key = public_key_file.readline()
+        if not args.no_github:
+            add_deploy_key(repository_name, public_key)
+        print_buffer += public_key
     print_buffer += '\n'
     ssh_config += f'Host github-{repository_name}\n' \
                   f'HostName github.com\n' \
@@ -57,8 +95,9 @@ print("""
 |__| |_____|___._|_____||__|__|__|_____|
 ========================================
 """)
-print('* Add the printed public keys (or in public-keys.txt) to the')
-print('  appropriate repositories (Repsitory/Setting/Deploy Keys)')
+if args.no_github:
+    print('* Add the printed public keys (or in public-keys.txt) to the')
+    print('  appropriate repositories (Repsitory/Setting/Deploy Keys)')
 print('* Move the generated keys to ~/.ssh (both private and public)')
 print('* Append ~/.ssh/config with the content in ssh-config.txt')
 print('* When use, Set the remote url to github-[repository_name].')
